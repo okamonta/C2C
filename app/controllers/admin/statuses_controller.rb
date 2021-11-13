eb106ba3fd1c8f389be7168e954ea38501760563
okamonta <kb24ko52@gmail.com>
2021-11-13 06:52:05 +0000

    Add genre status

--- app/controllers/admin/statuses_controller.rb
+++ app/controllers/admin/statuses_controller.rb
@@ -1,2 +1,31 @@
 class Admin::StatusesController < ApplicationController
+  
+  def index
+    @status = Status.new
+    @statuses = Status.all
+  end
+
+  def create
+    @status = Status.new(status_params)
+    if @status.save
+      redirect_to admin_statuses_path
+    else
+      render admin_statuses_path
+    end
+  end
+
+  def edit
+    @status = Status.find(params[:id])
+  end
+
+  def update
+    @status = Status.find(params[:id])
+    @status.update(status_params)
+    redirect_to admin_stautses_path
+  end
+  
+  def status_params
+    params.require(:status).permit(:name)
+  end
+  
 end
