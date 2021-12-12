aa802ac861c1050ac92451b1edf251bcb5c0101a
EC2 Default User <ec2-user@ip-172-31-20-131.ap-northeast-1.compute.internal>
2021-12-12 14:19:20 +0000

    Edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  root to: 'user/homes#top'
+  
   namespace :user do
     root to: 'homes#top'
     get 'homes/contact' => 'homes#contact'
